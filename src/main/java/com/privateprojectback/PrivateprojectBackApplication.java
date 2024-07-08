5cf4774f373c09f21ba9ebb8bee7e04a95dd8458
sehyeon <gs1884@naver.com>
2024-07-08 14:57:12 +0900

    first commit

---
+++ src/main/java/com/privateprojectback/PrivateprojectBackApplication.java
@@ -0,0 +1,13 @@
+package com.privateprojectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateprojectBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateprojectBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/privateprojectback/PrivateprojectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privateprojectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateprojectBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
