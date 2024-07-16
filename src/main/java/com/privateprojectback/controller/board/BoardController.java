6c059d6f11227237eaad04e1a5675b473a72a385
sehyeon <gs1884@naver.com>
2024-07-16 11:54:25 +0900

    글쓰기 기능

--- src/main/java/com/privateprojectback/controller/board/BoardController.java
+++ src/main/java/com/privateprojectback/controller/board/BoardController.java
@@ -1,5 +1,17 @@
 package com.privateprojectback.controller.board;
 
+import com.privateprojectback.domain.board.Board;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/board")
 public class BoardController {
 
+    @PostMapping("add")
+    public void add(@RequestBody Board board) {
+        System.out.println("board = " + board);
+    }
 }
---
+++ src/main/java/com/privateprojectback/domain/board/Board.java
@@ -0,0 +1,10 @@
+package com.privateprojectback.domain.board;
+
+import lombok.Data;
+
+@Data
+public class Board {
+    private String title;
+    private String content;
+    private String writer;
+}
