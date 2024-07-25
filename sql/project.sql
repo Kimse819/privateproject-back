87d4ceba3fe072a2c27a9a4fe7adf432978fb3f1
sehyeon <gs1884@naver.com>
2024-07-25 17:31:10 +0900

    영화 테이블 추가

--- sql/project.sql
+++ sql/project.sql
@@ -48,9 +48,63 @@ CREATE TABLE board_file
     PRIMARY KEY (board_id, name)
 );
 
-SELECT *
-FROM board
-ORDER BY id DESC;
+# 영화 테이블
+CREATE TABLE movie
+(
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    title        VARCHAR(100) NOT NULL,
+    description  TEXT         NOT NULL,
+    director     VARCHAR(50)  NOT NULL,
+    release_date DATE         NOT NULL,
+    duration     INT          NOT NULL, # Duration in minutes
+    inserted     DATETIME     NOT NULL DEFAULT NOW()
+);
+
+# 장르 테이블
+CREATE TABLE genre
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    name     VARCHAR(50) NOT NULL UNIQUE,
+    inserted DATETIME    NOT NULL DEFAULT NOW()
+);
+
+# 영화-장르 연결 테이블
+CREATE TABLE movie_genre
+(
+    movie_id INT NOT NULL REFERENCES movie (id),
+    genre_id INT NOT NULL REFERENCES genre (id),
+    PRIMARY KEY (movie_id, genre_id)
+);
+
+# 리뷰 테이블
+CREATE TABLE review
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    movie_id  INT      NOT NULL REFERENCES movie (id),
+    member_id INT      NOT NULL REFERENCES member (id),
+    content   TEXT     NOT NULL,
+    inserted  DATETIME NOT NULL DEFAULT NOW()
+);
+
+# 평점 테이블
+CREATE TABLE rating
+(
+    movie_id  INT      NOT NULL REFERENCES movie (id),
+    member_id INT      NOT NULL REFERENCES member (id),
+    score     INT      NOT NULL CHECK (score >= 1 AND score <= 10), # 평점은 1~10 사이
+    inserted  DATETIME NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (movie_id, member_id)
+);
+
+# 추천 테이블
+CREATE TABLE recommendation
+(
+    member_id INT      NOT NULL REFERENCES member (id),
+    movie_id  INT      NOT NULL REFERENCES movie (id),
+    reason    VARCHAR(255),
+    inserted  DATETIME NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (member_id, movie_id)
+);
 
 
 
